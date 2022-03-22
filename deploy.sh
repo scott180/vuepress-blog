bd78f6481d92dac47431f251326226fb2171fb5b
xuyongqin <1021151991@qq.com>
2022-03-22 09:45:44 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+echo 'www.vuepress-calligraphy.xushufa.cn' > CNAME
 
 git init
 git add -A
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:scott180/scott180.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:scott180/vuepress-blog.git master:gh-pages
+# git push -f git@github.com:scott180/vuepress-blog.git master:gh-pages
 
 cd -
