36e5c4e9e57e45dc15bc6aa9d916d41fe83091f6
xuyq <xuyongqin@hoomi.onaliyun.com>
2025-02-18 16:30:09 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,14 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-echo 'vuepress-blog.xushufa.cn' > CNAME
+# echo 'vuepress-blog.xushufa.cn' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:scott180/scott180.github.io.git master
+git push -f git@github.com:scott180/scott180.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:scott180/vuepress-blog.git master:gh-pages
